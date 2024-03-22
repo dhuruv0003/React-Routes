953e9bbc4353b118da782f98274f3f81f547afd2
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-23 01:06:28 +0530

    "new commit"

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,26 @@ import About from "./components/About.jsx"
 import Labs from "./components/Labs.jsx"
 import Support from "./components/Support.jsx" 
 import NotFound from "./components/NotFound.jsx"  
+import { NavLink,Link } from 'react-router-dom'
 
 function App() {
  
 
   return (
     <div className='App'>
+
+      <nav>
+        <ul>
+          {/* Link ki jagah NavLink isliye use karte hai kuki agar NavLink hai toh jis link par click krege usme active class add ho jati hai   */}
+
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/labs">Labs</NavLink></li>
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/support">Support</NavLink></li>
+        </ul>
+      </nav>
+
+
       {/* To create several routes embed emebed the route inside routes */}
       <Routes>
         <Route path="/" element={<Home/>}/>
